b070c6365801ac513c802ecc1fdccfc47cec5ab0
eggya <eggyachiquita@yahoo.com>
2013-03-29 15:57:03 +0000

    application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleIssueTracker::Application.config.secret_token = '1b5c9fff8fa6c961623c5b8c4f81141f800e2cb4526a256a7a3ec0e054d215c7b1e15594f0f8d35a159242be673c18962d924daddc0f313e5282bd20355db8d7'
